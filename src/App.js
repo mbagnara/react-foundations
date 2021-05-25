408059c4a2eda89ecb5e03ac4803aaff3c0f246f
mariobagnara <mbagnara@gmail.com>
2021-05-24 23:56:45 -0600

    Removed Commented Code - Left Usable Code

--- src/App.js
+++ src/App.js
@@ -10,22 +10,6 @@ class App extends React.Component {
         monsters: []
       }
 
-      // this.state = {
-      //   monsters: [
-      //     {
-      //       name: 'Frankenstein',
-      //       id: 'asc1'
-      //     },
-      //     {
-      //       name: 'Dracula',
-      //       id: 'erdf'
-      //     },
-      //     {
-      //       name: 'Zombie',
-      //       id: 'sdkl'
-      //     }
-      //   ]
-      // };
     }
 
     componentDidMount(){
@@ -48,65 +32,6 @@ class App extends React.Component {
   
     }    
 
-  // render(){
-
-  //   return (
-  //     <div className="App">
-  //       {this.state.monsters.map(
-  //         monster => (
-  //           <h1>{monster.name}</h1>
-  //         )
-  //       )}
-  //     </div>
-  //   );
-
-  // }
-
-  // constructor(){
-  //   super();
-  //   this.state = {
-  //     string: 'Hello Mario'
-  //   };
-  // }    
-
-  // render(){
-
-  //   return (
-  //     <div className="App">
-  //       <header className="App-header">
-  //         <img src={logo} className="App-logo" alt="logo" />
-  //         <p>{this.state.string}</p>
-  //         <button onClick={() => this.setState({ string: 'Hello There!'})}>Click me for a surprise!</button>
-  //       </header>
-  //     </div>
-  //   );
-
-  // }
-
-
 }
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React and Something Else.
-//           This is awesome how the page gets refreshed automatically upon saving the changes.
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
