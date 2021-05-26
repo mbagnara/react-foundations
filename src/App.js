906bb9b049ea7e966fa4208e8dd78fefafac0273
mariobagnara <mbagnara@gmail.com>
2021-05-25 23:16:16 -0600

    Added the filtering functionality

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
     constructor(){
       super();
       this.state = {
-        monsters: []
+        monsters: [],
+        searchField: ''
       }
 
     }
@@ -19,11 +20,35 @@ class App extends React.Component {
     }
 
     render(){
+      // this.setState() re-renders the page everytime it is called on causing to get a new list of filtered Monsters.
+      const { monsters, searchField } = this.state; // destructuring
+
+      // The above is equivalent to:
+      // const monsters = this.state.monsters;
+      // const searchField = this.state.searchField;
+
+      // This filters the monsters array based off the value of searchField.
+      const filteredMonsters = monsters.filter(monster =>
+          monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
 
       return (
         <div className="App">
+
+          <input 
+            type="search" 
+            placeholder="search monsters" 
+            onChange={e => {                      
+                this.setState(                    // Asynchronous Action. Second parameter executes synchronously
+                  {searchField: e.target.value},  // Set up the valyue of searchField
+                  () => console.log(this.state)   // (OPTIONAL) Called synchronously right after searchField is set up in the first parameter
+                ); 
+              }
+            }
+          />
+
           {/* We are passing monsters as a props to the CardList component */}
-          <CardList monsters={this.state.monsters} /> 
+          <CardList monsters={filteredMonsters} /> 
          </div>
       );
   
